df410f004c873a69c2eb0d07425b612c60f6e1ec
cheems-dev <daprimovaria@gmail.com>
2022-11-01 22:05:25 -0500

    feat(text-field): add new component text-field

---
+++ src/components/globals/TextField/TextField.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "setupTests";
+
+import TextField from "./TextField";
+
+/* describe("TextField", () => {
+  it("renders with default props", () => {
+    render(<TextField />);
+  });
+});
+ */
