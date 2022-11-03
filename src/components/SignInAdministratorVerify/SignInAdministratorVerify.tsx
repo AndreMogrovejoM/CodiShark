1809004d42bccfb6a3b2961e3ed61df622564a44
Andre Mogrovejo <andre.mogroveo@trade.ec>
2022-11-03 11:10:39 -0500

    chore(husky): husky config

--- src/components/SignInAdministratorVerify/SignInAdministratorVerify.tsx
+++ src/components/SignInAdministratorVerify/SignInAdministratorVerify.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import Styles from "./SignInAdministratorVerify.styles";
 import { SignInAdministratorVerifyProps as Props } from "./SignInAdministratorVerify.types";
 
-const SignInAdministratorVerify: React.FC<Props> = (props) => {
+const SignInAdministratorVerify: React.FC<Props> = props => {
   return (
     <Styles className="SignInAdministratorVerify">
       SignInAdministratorVerify
     </Styles>
-  )
+  );
 };
 
 SignInAdministratorVerify.defaultProps = {};
