10204d2b701365ca97b2b11030669e5b66d3b8f7
cheems-dev <daprimovaria@gmail.com>
2022-11-14 14:26:31 -0500

    refactor(payment-list): add navigate and use media querys to responsive data-table

--- src/components/userPanel/PaymentListView/PaymentListView.styles.tsx
+++ src/components/userPanel/PaymentListView/PaymentListView.styles.tsx
@@ -3,8 +3,6 @@ import styled from "styled-components";
 import { PaymentListViewStyledProps as Props } from "./PaymentListView.types";
 
 const PaymentListViewStyled = styled.div<Props>`
-  width: 64%;
-
   .PaymentListView {
   }
 `;
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -12,7 +12,9 @@ const CONSTANTS = {
     ENTRY_PATH: "/",
     ADMIN_ENTRY_PATH: "/administratorPanel",
     // Router where the user will land if not authenticated
-    NO_AUTH_PATH: "/signin"
+    NO_AUTH_PATH: "/signin",
+    // admin panel
+    USER_PANEL: "/userPanel"
   },
   API: {
     REACT_APP_API_URL: "https://cobranza.testcodishark.com/public/api",
