aaf9dac3571dc30c4040b197bb4f069fea3af45b
alearigth <diciembre.09.98@gmail.com>
2022-12-05 22:18:32 -0500

    feat(summary indicatorsclient): integration with services

--- src/components/IndicatorsClientOne/IndicatorsClientOne.tsx
+++ src/components/IndicatorsClientOne/IndicatorsClientOne.tsx
@@ -1,3 +1,4 @@
+import useAuth from "contexts/auth/auth.hooks";
 import useI18n from "i18n/i18n.hooks";
 import React from "react";
 
@@ -6,13 +7,15 @@ import { IndicatorsClientOneProps as Props } from "./IndicatorsClientOne.types";
 
 const IndicatorsClientOne: React.FC<Props> = props => {
   const t = useI18n().signIn.IndicatorsClientOne;
+  const { user } = useAuth();
+  const { first_name, last_name, mother_last_name } = user ?? {};
 
   return (
     <Styles className="IndicatorsClientOne">
       <div className="IndicatorsClientOne__component--card">
         <h2 className="IndicatorsClientOne__text--light">{t.greetings}</h2>
         <h2 className="IndicatorsClientOne__text--bold">
-          ARMANDO RODRIGUEZ GUERRA
+          {` ${first_name ?? ""}`} {` ${last_name ?? ""}`} {mother_last_name}
         </h2>
         <h2 className="IndicatorsClientOne__text--normal">DNI N° 86752459</h2>
       </div>
--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -4,12 +4,24 @@ import SummaryCard from "components/SummaryCard/SummaryCard";
 import Button from "components/globals/Button/Button";
 import useI18n from "i18n/i18n.hooks";
 import React from "react";
+import { useUserDebts } from "services/users/users.service.hooks";
 
 import Styles from "./Summary.styles";
 import { SummaryProps as Props } from "./Summary.types";
 
 const Summary: React.FC<Props> = props => {
   const t = useI18n().signIn.Summary;
+  const { data } = useUserDebts();
+  const { data: summaryData } = data ?? {};
+  const {
+    amount_cancellation,
+    amount_dscto_cancellation,
+    pct_dscto_cancellation,
+    date_last_contact,
+    currency,
+    banking_entity,
+    product
+  } = summaryData?.[0] ?? {};
 
   return (
     <Styles className="Summary">
@@ -17,16 +29,22 @@ const Summary: React.FC<Props> = props => {
       <div className="Summary__container--content">
         <div className="Summary__cardsContainer">
           <SummaryCard title={t.summary1}>
-            <h3 className="Summary__h3">S/ 8200</h3>
+            <h3 className="Summary__h3">{`S/. ${
+              amount_cancellation ?? ""
+            }`}</h3>
           </SummaryCard>
 
           <img src={lineSummary} className="Summary__image" alt="separator" />
 
           <SummaryCard title={t.summary2}>
             <div className="Summary__sectionDate">
-              <h3 className="Summary__h3 Summary__primary-color">S/6750.00</h3>
+              <h3 className="Summary__h3 Summary__primary-color">{`S/. ${
+                amount_dscto_cancellation ?? ""
+              }`}</h3>
               <div>
-                <span className="Summary__span">-35%</span>
+                <span className="Summary__span">{` - ${
+                  pct_dscto_cancellation ?? ""
+                } %`}</span>
               </div>
             </div>
           </SummaryCard>
@@ -34,7 +52,7 @@ const Summary: React.FC<Props> = props => {
           <img src={lineSummary} className="Summary__image" alt="separator" />
 
           <SummaryCard title={t.summary3}>
-            <h4 className="Summary__h4">10/09/2022</h4>
+            <h4 className="Summary__h4">{` ${date_last_contact ?? ""}`}</h4>
           </SummaryCard>
         </div>
 
@@ -47,6 +65,27 @@ const Summary: React.FC<Props> = props => {
             {t.button}
           </Button>
         </div>
+      </div>
+      <div className="Summary__section2">
+        <div></div>
+        <p className="Summary__p1">
+          Entidad Bancaria:{" "}
+          <span className="Summary__span1">{` ${banking_entity ?? ""}`}</span>
+        </p>
+        <p className="Summary__p1">
+          Tipo de Deuda:{" "}
+          <span className="Summary__span1">{` ${product ?? ""}`}</span>
+        </p>
+        <p className="Summary__p1">
+          Tipo de Moneda:{" "}
+          <span className="Summary__span1">{` ${currency ?? ""}`}</span>
+        </p>
+        <p className="Summary__p1">
+          Fecha:{" "}
+          <span className="Summary__span1">{` ${
+            date_last_contact ?? ""
+          }`}</span>
+        </p>
       </div>
     </Styles>
   );
